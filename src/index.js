ad02e87cc6438b7a438d863909c8f58db4669186
hashe <fmurcia1@toromail.csudh.edu>
2024-08-14 14:17:15 -0700

    fixing Landing page routing and added 404 prevention

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import Router
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Router>  {/* Removed the basename for simplicity */}
-            <App />
-        </Router>
+        <App />
     </React.StrictMode>
 );
 
