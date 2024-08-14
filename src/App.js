570772f3100a9e0390e9058d240033cfb8d7c5f1
hashe <fmurcia1@toromail.csudh.edu>
2024-08-13 17:54:35 -0700

    fixed landing page routing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import LandingPage from './Components/LandingPage';
 import AboutUs from './Components/About'; // Import the AboutUs component
-import Services from './Components/Services'
-import Pricing from './Components/Pricing'
-import Testimonial from './Components/Testimonials'
-import Clients from './Components/Clients'
-import Portfolio from './Components/Portfolio'
+import Services from './Components/Services';
+import Pricing from './Components/Pricing';
+import Testimonial from './Components/Testimonials';
+import Clients from './Components/Clients';
+import Portfolio from './Components/Portfolio';
 import './App.css';
 
 function App() {
@@ -17,11 +17,11 @@ function App() {
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/about" element={<AboutUs />} /> {/* AboutUs Route */}
-                <Route path="/services" element={<Services/>}/>
-                <Route path="/pricing" element={<Pricing/>}/>
-                <Route path="testimonials" element={<Testimonial/>}/>
-                <Route path="clients" element={<Clients/>}/>
-                <Route path="portfolio" element={<Portfolio/>}/>
+                <Route path="/services" element={<Services />} />
+                <Route path="/pricing" element={<Pricing />} />
+                <Route path="/testimonials" element={<Testimonial />} /> {/* Added leading slash */}
+                <Route path="/clients" element={<Clients />} /> {/* Added leading slash */}
+                <Route path="/portfolio" element={<Portfolio />} /> {/* Added leading slash */}
                 {/* Add more routes here as you build additional pages */}
             </Routes>
         </Router>
