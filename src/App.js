d92cc909fc17f56c2ed2fb5c1392966ee6fd6c8d
hashe <fmurcia1@toromail.csudh.edu>
2024-08-25 20:24:01 -0700

    updating contact page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import MobileNavbar from './Components/MobileNavbar'; // Import Mobile Navbar
+import MobileNavbar from './Components/MobileNavbar';
 import LandingPage from './Components/LandingPage';
 import AboutUs from './Components/About';
 import Services from './Components/Services';
@@ -9,11 +9,12 @@ import Pricing from './Components/Pricing';
 import Testimonial from './Components/Testimonials';
 import Clients from './Components/Clients';
 import Portfolio from './Components/Portfolio';
+import Contact from './Components/Contact';  // Import the Contact component
 import './App.css';
 
 function App() {
     return (
-        <Router> {/* Removed the basename prop */}
+        <Router>
             <Navbar />  {/* Desktop Navbar */}
             <MobileNavbar />  {/* Mobile Navbar */}
             <Routes>
@@ -24,6 +25,7 @@ function App() {
                 <Route path="/testimonials" element={<Testimonial />} />
                 <Route path="/clients" element={<Clients />} />
                 <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/contact" element={<Contact />} /> {/* Add the Contact route */}
             </Routes>
         </Router>
     );
